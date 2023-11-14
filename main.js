b964af578e2041161dd9aa9b36ba43814a328f39
DetGrey <anenovlar@gmail.com>
2023-11-14 17:50:58 +0100

    Unused code removed

--- main.js
+++ main.js
@@ -1,103 +1,3 @@
-const ctx = document.querySelector('#chart').getContext('2d');
-
-// const chart = new Chart(ctx, {
-//     type: 'scatter',
-//     data: {
-//         datasets: [{
-//             label: 'Houses',
-//             data: [
-//                 { x: 100, y: 1000000},
-//                 { x: 110, y: 2500000},
-//                 { x: 120, y: 2000000},
-//                 { x: 150, y: 3000000},
-//             ],
-//             backgroundColor: 'rgba(255, 99, 132)',
-//         }]
-//     },
-//     options: {
-//         onClick: (e) => {
-//             let activePoints = chart.getElementsAtEventForMode(e, 'nearest', { intersect: true }, false);
-//             if (activePoints.length > 0) {
-//                 let data = Chart.helpers.getRelativePosition(e, activePoints);
-//
-//                 const dataX = Math.round(chart.scales.x.getValueForPixel(data.x));
-//                 const dataY = Math.round(chart.scales.y.getValueForPixel(data.y)/500000)*500000;
-//
-//                 alert('house size: ' + dataX + '\n house price: ' + dataY)
-//             }
-//
-//         },
-//         plugins: {
-//             title: {
-//                 display: true,
-//                 text: 'House prices vs size',
-//                 font: {
-//                     size: 16,
-//                 },
-//             },
-//             legend: {
-//                 display: true,
-//                 labels: {
-//                     font: {
-//                         size: 16,
-//                     }
-//                 },
-//                 position: 'bottom',
-//             }
-//         },
-//         scales: {
-//             x: {
-//                 min: 90,
-//                 max: 160,
-//                 ticks: {
-//                     font: {
-//                         size: 16,
-//                     }
-//                 }
-//             },
-//             y: {
-//                 min: 1000000,
-//                 max: 3000000,
-//                 ticks: {
-//                     font: {
-//                         size: 16,
-//                     }
-//                 }
-//             }
-//         }
-//     }
-// });
-
-// fetch('https://unpkg.com/world-atlas/countries-50m.json').then((r) => r.json()).then((data) => {
-//     const countries = ChartGeo.topojson.feature(data, data.objects.countries).features;
-//
-//     const chart = new Chart(document.querySelector("#canvas").getContext("2d"), {
-//         type: 'choropleth',
-//         data: {
-//             labels: countries.map((d) => d.properties.name),
-//             datasets: [{
-//                 label: 'Countries',
-//                 data: countries.map((d) => ({feature: d, value: Math.random()})),
-//             }]
-//         },
-//         options: {
-//             showOutline: true,
-//             showGraticule: true,
-//             plugins: {
-//                 legend: {
-//                     display: false
-//                 },
-//             },
-//             scales: {
-//                 projection: {
-//                     axis: 'x',
-//                     projection: 'equalEarth'
-//                 }
-//             }
-//         }
-//     });
-// });
-
 // -------------------------------------------- HEAT MAP
 
 let map = L.map('map').setView([0,0], 1);
@@ -144,10 +44,10 @@ function onEachFeature(feature, layer) {
     }
 
     layer.on({
-         mouseover: function(e) {
+         mouseover: function() {
             layer.openPopup();
         },
-        mouseout: function(e) {
+        mouseout: function() {
             layer.closePopup();
         },
     });
